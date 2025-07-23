31addd74f93f0923d358eb406d527b9cf8e6c131
DolevPeretz <dolevop@gmail.com>
2025-07-23 11:33:28 +0300

    Removed secret and added .env.local to .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import SearchForm from "./components/form/searchForm";
 import UserProfile from "./components/user/UserProfile";
 const App = () => {
-  const [userName, setUserName] = useState("dolevPeretz");
+  const [userName, setUserName] = useState("quincylarson");
 
   return (
     <main className="mx-auto max-w-6xl px-8 py-20">
